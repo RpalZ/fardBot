4ab362ba81c94be6fad782f8fda0663b5c2b4884
RpalZ <72819391+RpalZ@users.noreply.github.com>
2021-07-24 19:43:36 +0300

    football

--- src/events/message.js
+++ src/events/message.js
@@ -7,7 +7,7 @@ module.exports = (client) => {
         if (!message.content.startsWith(prefix) || message.author.bot) return
         const args = message.content.slice(prefix.length).split(/ +/)
         const commandName = args.shift().toLowerCase()
-        const command = client.commands.get(commandName)
+        const command = client.commands.get(commandName) || client.command.find(f => f.aliases && f.aliases.includes(commandName))
 
         try {
             await command.execute(client, message, args, Discord)
