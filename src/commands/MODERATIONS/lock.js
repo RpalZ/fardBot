d7bbd722a27256f4493c4cd1ea136efdea8e9ed0
RpalZ <72819391+RpalZ@users.noreply.github.com>
2021-07-26 17:18:53 +0400

    defineUsr

--- src/commands/MODERATIONS/lock.js
+++ src/commands/MODERATIONS/lock.js
@@ -3,6 +3,8 @@ module.exports = {
     group: 'moderation',
     async execute(client, message, args) {
 
+        const user = message.member
+
         if (!user.hasPermission(['MANAGE_CHANNELS'])) return message.channel.send('You do not have permissions to run this command!'); //permissions
         if (!user.hasPermission(['ADMINISTRATOR'])) return message.channel.send('You do not have permissions to run this command!'); //permissions
 
@@ -10,14 +12,14 @@ module.exports = {
 
         try {
 
-            message.channel.updateOverwrite(message.guilds.roles.cache.find(e => e.name.toLowerCase().trim() == '@everyone'),{
+            message.channel.updateOverwrite(message.guilds.roles.cache.find(e => e.name.toLowerCase().trim() == '@everyone'), {
 
                 SEND_MESSAGES: false,
                 ADD_REACTIONS: false
             }) //setting the channels perms to not allow any messages
 
             msg.edit('**Successfully locked** the channel!') //sending the conformation message
-        } catch(e) {
+        } catch (e) {
             console.log(e) //console logging the error
         }
     }
--- src/commands/MODERATIONS/unlock.js
+++ src/commands/MODERATIONS/unlock.js
@@ -3,6 +3,8 @@ module.exports = {
     group: 'moderation',
     async execute(client, message, args) {
 
+        const user = message.member
+
         if (!user.hasPermission(['MANAGE_CHANNELS'])) return message.channel.send('You do not have permissions to run this command!'); //permissions
         if (!user.hasPermission(['ADMINISTRATOR'])) return message.channel.send('You do not have permissions to run this command!'); //permissions
 
@@ -10,14 +12,14 @@ module.exports = {
 
         try {
 
-            message.channel.updateOverwrite(message.guilds.roles.cache.find(e => e.name.toLowerCase().trim() == '@everyone'),{
+            message.channel.updateOverwrite(message.guilds.roles.cache.find(e => e.name.toLowerCase().trim() == '@everyone'), {
 
                 SEND_MESSAGES: true,
                 ADD_REACTIONS: true
             }) //setting the channels perms allow messages
 
             msg.edit('**Successfully Unlocked** the channel!') //sending the conformation message
-        } catch(e) {
+        } catch (e) {
             console.log(e) //console logging the error
         }
     }
