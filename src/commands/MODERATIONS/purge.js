fb8bf176afbcfe926dff3e3ebe392f915ccafc8a
RpalZ <72819391+RpalZ@users.noreply.github.com>
2021-07-26 17:12:20 +0400

    fixpurgeUserundefined

--- src/commands/MODERATIONS/purge.js
+++ src/commands/MODERATIONS/purge.js
@@ -3,16 +3,18 @@ module.exports = {
     group: 'moderation',
     async execute(client, message, args) {
 
+        const user = message.member
+
         if (!user.hasPermission(['MANAGE_MESSAGES'])) return message.channel.send('You do not have permissions to run this command!')
-        
 
-        if(!args[0]) return message.channel.send('Specify a number between 1 and 1000!'); //asking for a number between 1 and 1000
-        if(isNaN(args[0])) return message.channel.send('Enter a **number**') //asking for a number
-        if(parseInt(args[0]) > 1000) return message.channel.send('You can only delete **1000** messages at one time') //setting a limit to purge
 
-            await message.channel.bulkDelete(parseInt(args[0]) + 1).catch(err =>
-                console.log(err)) //deleting messages and looking for errors
-            
-        message.channel.send(`Deleted **${args[0]}** messages!`).then(m => m.delete({ timeout: 8000})) //sending conformation message
+        if (!args[0]) return message.channel.send('Specify a number between 1 and 1000!'); //asking for a number between 1 and 1000
+        if (isNaN(args[0])) return message.channel.send('Enter a **number**') //asking for a number
+        if (parseInt(args[0]) > 1000) return message.channel.send('You can only delete **1000** messages at one time') //setting a limit to purge
+
+        await message.channel.bulkDelete(parseInt(args[0]) + 1).catch(err =>
+            console.log(err)) //deleting messages and looking for errors
+
+        message.channel.send(`Deleted **${args[0]}** messages!`).then(m => m.delete({ timeout: 8000 })) //sending conformation message
     }
 }
