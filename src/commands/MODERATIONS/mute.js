569003f11f5025a9f8545965be6e89542d7c2787
RpalZ <72819391+RpalZ@users.noreply.github.com>
2021-07-26 18:10:32 +0400

    UnmuteAndMuteFixed

--- src/commands/MODERATIONS/mute.js
+++ src/commands/MODERATIONS/mute.js
@@ -2,39 +2,54 @@ const ms = require('ms')
 
 module.exports = {
     name: 'mute',
-    usage: '{MEMBER} {TIME}',
+    usage: '{MEMBER} {TIME:0d 0hr 0m 0s}',
     group: 'moderation',
     async execute(client, message, args) {
+
+        const user = message.member
 
         if (!user.hasPermission(['MUTE_MEMBERS'])) return message.channel.send('You do not have permissions to run this command!'); //setting permissions
         if (!user.hasPermission(['ADMINISTRATOR'])) return message.channel.send('You do not have permissions to run this command!'); //setting permissions
 
-        const target = message.mentions.users.first(); //Getting the user so we can mute
+        const guild = message.guild
 
-        if (target) {
+        const target = message.mentions.members.first(); //Getting the user so we can mute
+        const initDuration = args.slice(1).join(' ')
+        const duration = initDuration && initDuration.split(' ').reduce((acc, cur) => {
+            acc += ms(cur)
+            return acc
+        }, 0) || 0
+        if (isNaN(duration)) return message.channel.send('Please send a valid duration in the format: `0d 0hr 0m 0s`')
 
-            let mainRole = message.guild.roles.cache.find(role => role.name === 'community'); //setting the main role of the server
-            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted'); //setting the muted role of the server
 
-            let targetmember = message.guild.members.cache.get(target.id); //getting targets id
+        if (!target) return message.channel.send('That user is not in this guild!'); //se
 
-            if (args[1]) {
 
-                targetmember.roles.remove(mainRole.id); //removing main role
-                targetmember.roles.add(muteRole.id); //adding mute roke
-                message.channel.send(`<@${memberTarget.user.id}> has been muted`); //sending conformation that user has been muted
-                return
-            }
-            targetmember.roles.remove(mainRole.id); //removing main role
-            targetmember.roles.add(muteRole.id); //adding mute role
-            message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`); //sending conformation that user has been muted for x amount of time
+        const mutedRole = guild.roles.cache.find(m => m.name == 'Muted') || await guild.roles.create({
+            data: {
+                name: 'Muted',
+                position: 10,
+                hoist: false,
+                mentionable: false
 
-            setTimeout(function () {
-                targetmember.roles.remove(mainRole.id); //removing main role
-                targetmember.roles.add(muteRole.id); //adding mute role
-            }, ms(args[1]));
-        } else {
-            message.channel.send('That user is not in this guild!') //sending error message
+            },
         }
+        )
+
+        await target.roles.add(mutedRole, 'Man has been muted Down Bad!')
+
+        guild.channels.cache.map(m => {
+            m.updateOverwrite(mutedRole, {
+                SEND_MESSAGES: false
+            })
+        })
+
+
+        message.channel.send(duration > 0 ? `${target} has been muted for ${initDuration}.` : `${target} has been muted.`)
+
+        duration > 0 && setTimeout(async () => {
+            await target.roles.remove(mutedRole, 'Man has been unmuted still down bad!')
+        }, duration)
+
     }
 }
